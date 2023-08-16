8924e4bd873d5a1c715c24701bfb615fcd4f1082
soypo <136891264+soypo@users.noreply.github.com>
2023-08-16 22:26:56 +0900

    コードを変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+# bundle exec rake db:migrate(投稿していたデータは全て削除される→8行目を消す↓を9行目に追加)
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
